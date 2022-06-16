5d6959d5444f220c8a939afb1fd1f13abc850dad
tohidshabanloo <tshabanloo@hotmail.com>
2022-06-16 14:32:38 +0430

    1401.03.26 -1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c99b80f2e0185cb"; }
+/******/ 		__webpack_require__.h = function() { return "59755fbb697d7bcf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
